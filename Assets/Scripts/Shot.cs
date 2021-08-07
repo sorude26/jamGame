61c3d80daa0d69cbb45ac1cfd3c94f8e11a1978b
sorude26 <sorude26@gmail.com>
2021-08-07 15:21:07 +0900

    弾発射機能を追加した

---
+++ Assets/Scripts/Shot.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    [SerializeField] int m_damage = 1;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IDamage target = collision.GetComponent<IDamage>();
+        if (target != null)
+        {
+            target.Damage(m_damage);
+        }
+    }
+}
---
+++ Assets/Scripts/ShotControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotControl : MonoBehaviour
+{
+    [SerializeField] GameObject m_shotPrefab;
+    [SerializeField] string m_shotButton = "Shot";
+    [SerializeField] Transform m_muzzle;
+    [SerializeField] float m_shotInterval = 0.2f;
+    float m_shotTimer = 0;
+    private void Update()
+    {
+        if (Input.GetButton(m_shotButton))
+        {
+            m_shotTimer += Time.deltaTime;
+            if (m_shotTimer >= m_shotInterval)
+            {
+                var shot = Instantiate(m_shotPrefab);
+                shot.transform.position = m_muzzle.position;
+                shot.transform.rotation = this.transform.rotation;
+                m_shotTimer = 0;
+            }
+        }
+        else if(m_shotTimer != m_shotInterval)
+        {
+            m_shotTimer = m_shotInterval;
+        }
+    }
+}
