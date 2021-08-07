9dcb5b798d1179b36ccf2206421724891a137790
AkimotoYuhi <chiisanaaki@gmail.com>
2021-08-07 13:57:57 +0900

    指定方向に進むスクリプトを作成した

---
+++ Assets/Scripts/MoveStraight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveStraight : MoveControl
+{
+    /// <summary>進行方向</summary>
+    [SerializeField] public Vector2 m_vec;
+    /// <summary>移動速度</summary>
+    [SerializeField] private float m_speed = 5f;
+    private Rigidbody2D m_rb;
+    private bool m_move;
+    void Start()
+    {
+        m_rb = GetComponent<Rigidbody2D>();
+        m_move = true;
+    }
+
+    void Update()
+    {
+        if (!m_move)
+        {
+            m_vec = Vector2.zero;
+        }
+        m_rb.velocity = new Vector2(m_vec.x * m_speed, m_vec.y * m_speed);
+    }
+
+    public override void GameEnd()
+    {
+        throw new System.NotImplementedException();
+    }
+}
