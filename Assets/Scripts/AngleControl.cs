906ab3b8198f303f8ab42a6e18e5c9972eb085b5
sorude26 <sorude26@gmail.com>
2021-08-07 15:21:30 +0900

    方向とライフを管理するスクリプトを追加した

---
+++ Assets/Scripts/AngleControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngleControl : MonoBehaviour
+{
+    [SerializeField] string m_inputHname = "Horizontal";
+    [SerializeField] string m_inputVname = "Vertical";
+    void Update()
+    {
+        float x = Input.GetAxisRaw(m_inputHname);
+        float y = Input.GetAxisRaw(m_inputVname);
+        transform.up = new Vector2(x, y).normalized;
+    }
+}
---
+++ Assets/Scripts/LifeControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeControl : MonoBehaviour,IDamage
+{
+    [SerializeField] int m_maxLife = 10;
+    public int Life { get; private set; }
+    private void Start()
+    {
+        Life = m_maxLife;
+    }
+    public virtual void Damage(int damage)
+    {
+        if (Life <= 0)
+        {
+            return;
+        }
+        Life -= damage;
+        if (Life <= 0)
+        {
+            Dead();
+        }
+    }
+    public virtual void Dead()
+    {
+        Destroy(this.gameObject);
+    }
+}
