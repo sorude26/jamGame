10205b5085801fdbcd9a1ca2213354b2ac0ff3a3
AkimotoYuhi <chiisanaaki@gmail.com>
2021-08-07 16:05:40 +0900

    爆弾の作成とＨＰゲージのプレハブ化をした

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : LifeControl
+{
+    [SerializeField] private int m_bulletNum = 50;
+    [SerializeField] private GameObject m_bullet;
+    public override void Dead()
+    {
+        for (int i = 0; i < m_bulletNum; i++)
+        {
+            float random = Random.Range(0f, 360f);
+            Instantiate(m_bullet, gameObject.transform.position, Quaternion.Euler(0, 0, random));
+            
+        }
+        base.Dead();
+    }
+}
